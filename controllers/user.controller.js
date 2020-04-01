89b9e26aa4871904a069584bec2e14f722f3ad89
K0o97 <rihood.sec@gmail.com>
2020-04-01 15:22:55 +0700

    Create user validation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -20,6 +20,20 @@ module.exports.getCreate = (req, res) => res.render('users/create');
 
 module.exports.postCreate = (req, res) => {
     req.body.id = shortid.generate();
+    var errors = [];
+    if (!req.body.name) {
+        errors.push("Name is required.");
+    }
+    if (!req.body.phonenumber) {
+        errors.push("Phone number is required.");
+    }
+    if(errors.length) {
+        res.render('users/create', {
+            errors: errors,
+            values: req.body
+        });
+        return;
+    }
     db.get('users').push(req.body).write();
     res.redirect('/users');
 };
