17e02ddf5a3050d057b89e1488951b6adbcdf73c
K0o97 <rihood.sec@gmail.com>
2020-04-01 14:20:47 +0700

    static file

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.set('view engine', 'pug');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(express.static('public'));
 
 app.get('/', (req, res) => res.render('index'));
 
